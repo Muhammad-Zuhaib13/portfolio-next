8fdc61fcc2e877527a7a742890584ce79de05dc0
Muhammad-Zuhaib13 <muhammadzohaib578@gmail.com>
2024-06-24 15:56:25 +0500

    updated lufga fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lufgalight: ["var(--font-lufga-light)"],
+        lufgamedium: ["var(--font-lufga-medium)"],
+        lufgasemibold: ["var(--font-lufga-semi-bold)"],
+        lufgabold: ["var(--font-lufga-bold)"],
       },
     },
   },
